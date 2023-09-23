3860c421c7b4bd1f4390021ecd5b74b7ed33f868
Vbuuu <87385391+Vbuuu@users.noreply.github.com>
2023-09-23 14:14:59 +0200

    first commit

---
+++ src/data/index.js
@@ -0,0 +1,54 @@
+const iconUrl =
+  'https://raw.githubusercontent.com/devicons/devicon/master/icons/';
+
+export const projects = [
+  {
+    name: 'Periodictable',
+    description: 'Goofy augghh website with the periodic table by Max and TKV',
+    link: 'https://github.com/Vbuuu/periodictable',
+    languages: [
+      {
+        name: 'Javascript',
+        icon: iconUrl + 'javascript/javascript-plain.svg',
+      },
+      {
+        name: 'Html',
+        icon: iconUrl + 'html5/html5-plain.svg',
+      },
+      {
+        name: 'Css',
+        icon: iconUrl + 'css3/css3-plain.svg',
+      },
+    ],
+  },
+  {
+    name: 'Vopenheimer',
+    description:
+      'A very basic prototype of me trying to create the Copenheimer bot',
+    link: 'https://github.com/Vbuuu/Vopenheimer',
+    languages: [
+      {
+        name: 'Python',
+        icon: iconUrl + 'python/python-plain.svg',
+      },
+    ],
+  },
+  {
+    name: 'Portfolio',
+    description: 'This is my self coded portfolio using Astro',
+    link: 'https://github.com/Vbuuu/portfolio',
+    languages: [
+      {
+        name: 'Astro',
+      },
+      {
+        name: 'Javascript',
+        icon: iconUrl + 'javascript/javascript-plain.svg',
+      },
+      {
+        name: 'Css',
+        icon: iconUrl + 'css3/css3-plain.svg',
+      },
+    ],
+  },
+];
---
+++ src/pages/blog/post-1.md
@@ -0,0 +1,25 @@
+---
+layout: '../../layouts/BlogPostLayout.astro'
+title: 'Mein erster Blogpost'
+author: 'Max'
+pubDate: '20 Dez, 2023'
+---
+
+Gestern habe ich mit diesem Projekt angefangen, da ich eine Aufgabe in Mathe-Informatik bekommen habe. Die Aufgabe war es, eine Website zu programmieren.
+Diese Website sollte Bilder, Links, CSS, Javascript enthalten sollte.
+Ich habe mich entschieden, diese Website mit [Astro](https://astro.build) zu bauen.
+
+## Was ist Astro
+
+Astro ist ein leistungsstarkes Javascript-Framework, mit starkem Fokus auf Geschwindigkeit. Mit seiner wachsenden Community ist [Astro](https://astro.build) auf jeden Fall einen Blick wert, wenn es darum geht moderne Webseiten zu erstellen.
+
+## Was ich bis jetzt geschafft habe
+
+Ich habe bis jetzt eine [Hauptseite](/) und eine [Seite mit meinen Projekten](/work) geschrieben, welche ich mithilfe einer Json Liste jederzeit ganz einfach verändern kann.
+Zwei [Layouts](https://docs.astro.build/de/core-concepts/layouts/) habe ich auch erstellt, um ganz einfach neue Seiten erstellen zu können.
+
+Und noch vieles mehr, was man alles hier erkunden kann.
+
+## Meine Ziele für meine Website
+
+Mein Ziel ist es eine Portfolio-website zu erstellen, wo ich ab und zu einen oder zwei Blogposts erstelle.
